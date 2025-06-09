ae48f8d56ff26dc867dfea1487dad1cd2cb63590
Marat Babaev <maratbabaev04@mail.ru>
2025-06-10 01:43:05 +0300

    Добавьте файлы проекта.

---
+++ EventRegistry/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace EventRegistrationApp
+{
+    public partial class MainWindow : Window
+    {
+        string connectionString = "Data Source=DESKTOP-UFRBR18;Initial Catalog=EventRegistryDB;Integrated Security=True;";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OrganizerButton_Click(object sender, RoutedEventArgs e)
+        {
+            new OrganizerWindow().Show();
+            Close();
+        }
+
+        private void ParticipantButton_Click(object sender, RoutedEventArgs e)
+        {
+            new ParticipantWindow().Show();
+            Close();
+        }
+
+        private void RegistrarButton_Click(object sender, RoutedEventArgs e)
+        {
+            new RegistrarWindow().Show();
+            Close();
+        }
+        private void TitleBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ EventRegistry/Models/Attendance.cs
@@ -0,0 +1,15 @@
+﻿using EventRegistrationApp;
+using System;
+
+public class Attendance
+{
+    public int Id { get; set; }
+    public Participant Participant { get; set; }
+    public Event Event { get; set; }
+    public bool IsPresent { get; set; }
+    public override string ToString()
+    {
+        return $"{Participant?.Name}    -    {DateTime.Now:dd.MM.yyyy}";
+    }
+
+}
